417c58cf7c2a17860de42c77aa4da5db5c25e280
doyleyeh <doyleyeh@gmail.com>
2025-04-21 20:17:15 -0400

    fix basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/doyleyeh.github.io' : '',
+  basePath: '',
 };
 
 module.exports = nextConfig; 
